bbe9a913a41fe3c3e22ae56807bf9c8942764032
Lachlan Moody <ltmoo2@student.monash.edu>
2020-08-29 15:26:59 +1000

    Added plots to shiny app and added user controls. Changed to recode rather than mutate

--- 27809951_Lachlan_Moody/app.R
+++ 27809951_Lachlan_Moody/app.R
@@ -12,18 +12,20 @@ Counts <- read_csv("data/Pedestrian_Counting_System_2019 (Exercise 2).csv")
 Counts$Day <- factor(Counts$Day, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
 
 
-Counts <- Counts %>%
-    mutate(Sensor_Name = if_else(Sensor_Name == "Collins St (North)", "Collins Street (North)", Sensor_Name)) %>%
-    mutate(Sensor_Name = if_else(Sensor_Name == "Flinders la - Swanston St (West) Temp", "Flinders La - Swanston St (West) Temporary", Sensor_Name)) %>%
-    mutate(Sensor_Name = if_else(Sensor_Name == "Flinders La - Swanston St (West) Temp", "Flinders La - Swanston St (West) Temporary", Sensor_Name)) %>%
-    mutate(Sensor_Name = if_else(Sensor_Name == "Lincoln-Swanston(West)", "Lincoln-Swanston (West)", Sensor_Name)) %>%
-    mutate(Sensor_Name = if_else(Sensor_Name == "Pelham St (S)", "Pelham St (South)", Sensor_Name))
+Counts$Sensor_Name <- recode(Counts$Sensor_Name,
+                             "Collins St (North)" = "Collins Street (North)",
+                             "Flinders la - Swanston St (West) Temp" = "Flinders La - Swanston St (West) Temporary",
+                             "Flinders La - Swanston St (West) Temp" = "Flinders La - Swanston St (West) Temporary",
+                             "Lincoln-Swanston(West)" = "Lincoln-Swanston (West)",
+                             "Pelham St (S)" = "Pelham St (South)"
+)
 
+Locations$sensor_name <- recode(Locations$sensor_name,
+                                "Melbourne Central-Elizabeth St (East)Melbourne Central-Elizabeth St (East)" = "Melbourne Central-Elizabeth St (East)",
+                                "Building 80 RMIT" = "Swanston St - RMIT Building 80",
+                                "RMIT Building 14" = "Swanston St - RMIT Building 14"
+)
 
-Locations <-  Locations %>%
-    mutate(sensor_name = if_else(sensor_name == "Melbourne Central-Elizabeth St (East)Melbourne Central-Elizabeth St (East)", "Melbourne Central-Elizabeth St (East)", sensor_name)) %>%
-    mutate(sensor_name = if_else(sensor_name == "Building 80 RMIT", "Swanston St - RMIT Building 80", sensor_name)) %>%
-    mutate(sensor_name = if_else(sensor_name == "RMIT Building 14", "Swanston St - RMIT Building 14", sensor_name))
 
 
 Total_Counts <- Counts %>%
@@ -31,7 +33,7 @@ Total_Counts <- Counts %>%
     summarise(average = mean(Hourly_Counts))
 
 
-Combined <- Total_Counts %>% 
+Combined <- Total_Counts %>%
     left_join(Locations, by = c("Sensor_Name" = "sensor_name"))
 
 
@@ -45,21 +47,25 @@ Daily_Counts <- Counts %>%
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel(
+        h1("FIT5147 Programming Exercise 2: R", align = "center")
+        ),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(position = "right",
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("select",
+                        "Select sensor",
+                        choices = unique(Combined$Sensor_Name),
+                        selected = "Alfred Place")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           h2("Mapping Melbourne pedestrian counts"),
+           leafletOutput("mymap"),
+           h2("Average hourly counts for 2019 per day"),
+           plotOutput("myplot")
         )
     )
 )
@@ -67,15 +73,27 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$mymap <- renderLeaflet({
+        Combined %>%
+            leaflet() %>%
+            addProviderTiles("CartoDB.DarkMatter") %>%
+            addCircles(~longitude, ~latitude,
+                       radius = ~average/20,
+                       label = ~as.character(Sensor_Name),
+                       col = "pink")
+    })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$myplot <- renderPlot({
+        Plot_Daily <- Daily_Counts %>%
+            filter(Sensor_Name == input$select) %>%
+            ggplot(aes(x = Time,
+                       y = average,
+                       group = Sensor_Name)) +
+            geom_line() +
+            facet_wrap(~Day)
+        print(Plot_Daily)
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
